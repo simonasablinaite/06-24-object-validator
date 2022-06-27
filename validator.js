77bdcca55bd873c982f259fbbb2617dfbf9039da
SiMONA22 <100865207+simonasablinaite@users.noreply.github.com>
2022-06-27 15:39:06 +0300

    Initial commit

---
+++ validator.js
@@ -0,0 +1,12 @@
+/**
+ * Duomenu objekto validatorius, kuris tikrina, ar duomenu objekte yra tik leistini raktazodziai
+ * 
+ * Objekte gali buti ir papildomu, neprivalomu, bet vis vien leistinu raktazodziu (obtional keys)
+ * @param {Object} obj Duomenu objektas
+ * @returns {[boolean, string]} Rezultatas, kur pirmasis parametras reiskia ar buvo rasta klaida, o antrasis - zinute (aprasanti klaida)
+ */
+function validator(obj) {
+
+}
+
+module.exports = validator;
