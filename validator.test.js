a8d7d166eeb24cf008d61c400246a7d70899495b
SiMONA22 <100865207+simonasablinaite@users.noreply.github.com>
2022-06-28 09:59:54 +0300

    Bad types of tests

--- validator.test.js
+++ validator.test.js
@@ -1,1 +1,76 @@
 const validator = require('./validator.js');
+
+describe('Blogi tipai', () => {
+  test('empty', () => {
+    const [err, msg] = validator();
+    expect(err).toBe(true);
+    expect(msg).toBe('Neduotas objektas');
+  })
+
+  test('number', () => {
+    const [err, msg] = validator(1);
+    expect(err).toBe(true);
+    expect(msg).toBe('Neduotas objektas');
+  })
+
+  test('string', () => {
+    const [err, msg] = validator('');
+    expect(err).toBe(true);
+    expect(msg).toBe('Neduotas objektas');
+  })
+
+  test('boolean', () => {
+    const [err, msg] = validator(true);
+    expect(err).toBe(true);
+    expect(msg).toBe('Neduotas objektas');
+  })
+
+  test('null', () => {
+    const [err, msg] = validator(null);
+    expect(err).toBe(true);
+    expect(msg).toBe('Neduotas objektas');
+  })
+
+  test('array', () => {
+    const [err, msg] = validator([]);
+    expect(err).toBe(true);
+    expect(msg).toBe('Neduotas objektas');
+  })
+
+  test('no structure', () => {
+    const [err, msg] = validator({});
+    expect(err).toBe(true);
+    expect(msg).toBe('Neduotas strukturos objektas');
+  })
+
+  test('structure: number', () => {
+    const [err, msg] = validator({}, 1);
+    expect(err).toBe(true);
+    expect(msg).toBe('Neduotas strukturos objektas');
+  })
+
+  test('structure: string', () => {
+    const [err, msg] = validator({}, '');
+    expect(err).toBe(true);
+    expect(msg).toBe('Neduotas strukturos objektas');
+  })
+
+  test('structure: boolean', () => {
+    const [err, msg] = validator({}, false);
+    expect(err).toBe(true);
+    expect(msg).toBe('Neduotas strukturos objektas');
+  })
+
+  test('structure: null', () => {
+    const [err, msg] = validator({}, null);
+    expect(err).toBe(true);
+    expect(msg).toBe('Neduotas strukturos objektas');
+  })
+
+  test('structure: aarray', () => {
+    const [err, msg] = validator({}, []);
+    expect(err).toBe(true);
+    expect(msg).toBe('Neduotas strukturos objektas');
+  })
+
+})
